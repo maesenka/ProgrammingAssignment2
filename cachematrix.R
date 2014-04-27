c95c6af5b95fa8b9b582ebf86db56581118eec49
Karel Maesen <karel@geovise.com>
2014-04-27 14:48:41 +0200

    Completes the cachematrix functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate and cache the inverse of a matrix. When
+## the inverse has been calculated once using cacheSolve, any subsequent invocation
+## of this function returns the cached value.
+## 
 
-## Write a short comment describing this function
-
+## Creates a matrix that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        # initialize the inverse cache to NULL
+        inv <- NULL
+        set <- function(y) {
+          x <<- y
+          inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse = setinverse, getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
-
+## Calculates the inverse of the given matrix, and stores it in the matrix cache
 cacheSolve <- function(x, ...) {
+        inv <- x$getinverse()
+        if (!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+        }
+        data <- x$get()
+        inv <- 1/data
+        x$setinverse(inv)        
         ## Return a matrix that is the inverse of 'x'
+        inv
 }
